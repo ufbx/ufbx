68ee7e8e3db1929a29aac227171275ed039955b1
bqqbarbhg <samuli.1995@hotmail.com>
2020-01-06 15:49:13 +0200

    Add deflate fuzzer

---
+++ misc/fuzz_deflate.c
@@ -0,0 +1,42 @@
+#define ufbx_assert(cond) do { \
+		if (!(cond)) __builtin_trap(); \
+	} while (0)
+#include "../ufbx_implementation.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	if (argc < 2) {
+		fprintf(stderr, "Usage: fuzz_deflate <file>\n");
+		return 1;
+	}
+	FILE *f = fopen(argv[1], "rb");
+	if (!f) {
+		fprintf(stderr, "Failed to open file\n");
+		return 1;
+	}
+	fseek(f, 0, SEEK_END);
+	size_t src_size = ftell(f);
+	fseek(f, 0, SEEK_SET);
+	char *src = malloc(src_size);
+	size_t num_read = fread(src, 1, src_size, f);
+	if (num_read != src_size) {
+		fprintf(stderr, "Failed to read file\n");
+		return 1;
+	}
+	fclose(f);
+
+	size_t dst_size = 1024*src_size;
+	char *dst = malloc(dst_size);
+
+	ptrdiff_t result = ufbxi_inflate(dst, dst_size, src, src_size);
+
+	free(src);
+	free(dst);
+
+	printf("%td\n", result);
+	return 0;
+}
+
