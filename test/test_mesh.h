ccfd2201417c04af8a47c8c84124ff8fe293183a
bqqbarbhg <samuli.1995@hotmail.com>
2020-03-31 20:52:53 +0300

    Add Blender UV/Color set tests

--- test/test_mesh.h
+++ test/test_mesh.h
@@ -118,6 +118,80 @@ UFBXT_FILE_TEST(maya_uv_sets)
 }
 #endif
 
+UFBXT_FILE_TEST(blender_279_color_sets)
+#if UFBXT_IMPL
+{
+	ufbx_mesh *mesh = ufbx_find_mesh(scene, "Cube");
+	ufbxt_assert(mesh);
+	ufbxt_assert(mesh->color_sets.size == 3);
+	ufbxt_assert(!strcmp(mesh->color_sets.data[0].name.data, "RGBCube"));
+	ufbxt_assert(!strcmp(mesh->color_sets.data[1].name.data, "White"));
+	ufbxt_assert(!strcmp(mesh->color_sets.data[2].name.data, "Black"));
+
+	for (size_t i = 0; i < mesh->num_indices; i++) {
+		ufbx_vec3 pos = ufbx_get_vertex_vec3(&mesh->vertex_position, i);
+		ufbx_vec4 refs[3] = {
+			{ 0.0, 0.0, 0.0, 1.0 },
+			{ 1.0, 1.0, 1.0, 1.0 },
+			{ 0.0, 0.0, 0.0, 1.0 },
+		};
+
+		refs[0].x = pos.x + 0.5;
+		refs[0].y = pos.y + 0.5;
+		refs[0].z = pos.z + 0.5;
+
+		for (size_t set_i = 0; set_i < 3; set_i++) {
+			ufbx_vec4 color = ufbx_get_vertex_vec4(&mesh->color_sets.data[set_i].vertex_color, i);
+			ufbxt_assert_close_vec4(err, color, refs[set_i]);
+		}
+	}
+}
+#endif
+
+UFBXT_FILE_TEST(blender_279_uv_sets)
+#if UFBXT_IMPL
+{
+	ufbx_mesh *mesh = ufbx_find_mesh(scene, "Cube");
+	ufbxt_assert(mesh);
+	ufbxt_assert(mesh->uv_sets.size == 3);
+	ufbxt_assert(!strcmp(mesh->uv_sets.data[0].name.data, "Default"));
+	ufbxt_assert(!strcmp(mesh->uv_sets.data[1].name.data, "PerFace"));
+	ufbxt_assert(!strcmp(mesh->uv_sets.data[2].name.data, "Row"));
+
+	size_t counts1[2][2] = { 0 };
+	size_t counts2[7][2] = { 0 };
+
+	for (size_t i = 0; i < mesh->num_indices; i++) {
+		ufbx_vec2 uv0 = ufbx_get_vertex_vec2(&mesh->uv_sets.data[0].vertex_uv, i);
+		ufbx_vec2 uv1 = ufbx_get_vertex_vec2(&mesh->uv_sets.data[1].vertex_uv, i);
+		ufbx_vec2 uv2 = ufbx_get_vertex_vec2(&mesh->uv_sets.data[2].vertex_uv, i);
+
+		ufbxt_assert(uv0.x > 0.05f && uv0.y > 0.05f && uv0.x < 0.95f && uv0.y < 0.95f);
+		int x1 = (int)(uv1.x + 0.5f), y1 = (int)(uv1.y + 0.5f);
+		int x2 = (int)(uv2.x + 0.5f), y2 = (int)(uv2.y + 0.5f);
+		ufbxt_assert_close_real(err, uv1.x - (ufbx_real)x1, 0.0);
+		ufbxt_assert_close_real(err, uv1.y - (ufbx_real)y1, 0.0);
+		ufbxt_assert_close_real(err, uv2.x - (ufbx_real)x2, 0.0);
+		ufbxt_assert_close_real(err, uv2.y - (ufbx_real)y2, 0.0);
+		ufbxt_assert(x1 >= 0 && x1 <= 1 && y1 >= 0 && y1 <= 1);
+		ufbxt_assert(x2 >= 0 && x2 <= 6 && y2 >= 0 && y2 <= 1);
+		counts1[x1][y1]++;
+		counts2[x2][y2]++;
+	}
+
+	ufbxt_assert(counts1[0][0] == 6);
+	ufbxt_assert(counts1[0][1] == 6);
+	ufbxt_assert(counts1[1][0] == 6);
+	ufbxt_assert(counts1[1][1] == 6);
+
+	for (size_t i = 0; i < 7; i++) {
+		size_t n = (i == 0 || i == 6) ? 1 : 2;
+		ufbxt_assert(counts2[i][0] == n);
+		ufbxt_assert(counts2[i][1] == n);
+	}
+}
+#endif
+
 UFBXT_FILE_TEST(synthetic_sets_reorder)
 #if UFBXT_IMPL
 {
