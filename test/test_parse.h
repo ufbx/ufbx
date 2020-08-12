ce46c2c2424ffa14297732758759d5d60c99f572
bqqbarbhg <samuli.1995@hotmail.com>
2020-08-12 22:10:42 +0300

    Add test for ASCII leading comma

--- test/test_parse.h
+++ test/test_parse.h
@@ -1,4 +1,11 @@
 
+UFBXT_FILE_TEST(maya_leading_comma)
+#if UFBXT_IMPL
+{
+	ufbxt_assert(!strcmp(scene->metadata.creator.data, "FBX SDK/FBX Plugins version 2019.2"));
+}
+#endif
+
 UFBXT_FILE_TEST(maya_zero_end)
 #if UFBXT_IMPL
 {
