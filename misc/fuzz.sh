8fd1d8ba6eab1552809bee2e26a457c390eeb816
bqqbarbhg <samuli.1995@hotmail.com>
2021-07-16 03:15:58 +0300

    Use discrete allocations with ASAN fuzzing

--- misc/fuzz.sh
+++ misc/fuzz.sh
@@ -5,11 +5,12 @@ if [ "$1" == "build" ]; then
 elif [ "$1" == "build-32" ]; then
     afl-clang-fast -static ../../misc/fuzz_ufbx_persist.c -lm -o fuzz_ufbx_32
 elif [ "$1" == "build-asan" ]; then
-    AFL_USE_ASAN=1 afl-clang-fast ../../misc/fuzz_ufbx_persist.c -lm -o fuzz_ufbx_asan
+    AFL_USE_ASAN=1 afl-clang-fast -DDISCRETE_ALLOCATIONS ../../misc/fuzz_ufbx_persist.c -lm -o fuzz_ufbx_asan
 elif [ "$1" == "build-asan-32" ]; then
-    AFL_USE_ASAN=1 afl-clang-fast ../../misc/fuzz_ufbx_persist.c -lm -o fuzz_ufbx_asan_32
+    AFL_USE_ASAN=1 afl-clang-fast -DDISCRETE_ALLOCATIONS ../../misc/fuzz_ufbx_persist.c -lm -o fuzz_ufbx_asan_32
 fi
 
 if [ "$1" == "fuzz" ]; then
-    afl-fuzz -i cases -o "$2_findings" -t 1000 -m 2000 "./$2"
+    cp -r cases "cases_$2"
+    afl-fuzz -i "cases_$2" -o "findings_$2" -t 1000 -m 2000 "./$2"
 fi
