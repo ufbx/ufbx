59f34b5ad2cd6dcc7f2c5efbf2ef057e6c48a812
bqqbarbhg <samuli.1995@hotmail.com>
2021-07-17 16:27:52 +0300

    Coverage fixes

--- misc/generate_coverage.sh
+++ misc/generate_coverage.sh
@@ -4,4 +4,5 @@ set -x
 clang -lm -coverage -g -std=gnu99 ufbx.c test/runner.c -o build/cov-runner
 build/cov-runner -d data
 llvm-cov gcov ufbx runner -b
+chmod +x misc/llvm_gcov.sh
 lcov --directory . --base-directory . --gcov-tool misc/llvm_gcov.sh --rc lcov_branch_coverage=1 --capture -o coverage.lcov
