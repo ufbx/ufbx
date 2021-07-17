c179d37cf425c43548c7fe867b6ef6e8bff7ce70
bqqbarbhg <samuli.1995@hotmail.com>
2021-07-17 16:58:06 +0300

    Supply absolute path to llvm_gcov.sh

--- misc/generate_coverage.sh
+++ misc/generate_coverage.sh
@@ -3,9 +3,10 @@ set -x
 set -e
 
 LLVM_COV="${LLVM_COV:-llvm-cov}"
+LLVM_GCOV=$(realpath misc/llvm_gcov.sh)
+chmod +x misc/llvm_gcov.sh
 
 clang -lm -coverage -g -std=gnu99 ufbx.c test/runner.c -o build/cov-runner
 build/cov-runner -d data
 $LLVM_COV gcov ufbx runner -b
-chmod +x misc/llvm_gcov.sh
-lcov --directory . --base-directory . --gcov-tool misc/llvm_gcov.sh --rc lcov_branch_coverage=1 --capture -o coverage.lcov
+lcov --directory . --base-directory . --gcov-tool $LLVM_GCOV --rc lcov_branch_coverage=1 --capture -o coverage.lcov
