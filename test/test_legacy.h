7c99dc0e6415cb1a04f5c1a6a09467f599604f2d
bqqbarbhg <samuli.1995@hotmail.com>
2021-08-30 00:03:54 +0300

    max2009_blob: Check for node children

--- test/test_legacy.h
+++ test/test_legacy.h
@@ -2,6 +2,10 @@
 UFBXT_FILE_TEST(max2009_blob)
 #if UFBXT_IMPL
 {
+	ufbx_node *node = ufbx_find_node(scene, "Box02");
+	ufbxt_assert(node);
+	ufbxt_assert(node->mesh);
+	ufbxt_assert(node->children.count == 16);
 }
 #endif
 
