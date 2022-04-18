c8f54b07657765c115497112a2a1ae9c9ef72fb9
bqqbarbhg <samuli.1995@hotmail.com>
2022-04-18 14:59:04 +0300

    Dataset testing base

---
+++ misc/check_dataset.py
@@ -0,0 +1,92 @@
+import os
+import json
+from typing import NamedTuple, Optional
+import subprocess
+
+class TestCase(NamedTuple):
+    root: str
+    fbx_path: str
+    obj_path: Optional[str]
+    mtl_path: Optional[str]
+    title: str
+    author: str
+    license: str
+    url: str
+
+def gather_dataset_tasks(root_dir):
+    for root, _, files in os.walk(root_dir):
+        for filename in files:
+            if not filename.endswith(".json"):
+                continue
+
+            path = os.path.join(root, filename)
+            with open(path, "rt", encoding="utf-8") as f:
+                desc = json.load(f)
+            
+            fbx_path = path.replace(".json", ".fbx")
+            assert os.path.exists(fbx_path)
+
+            obj_path = path.replace(".json", ".obj")
+            if not os.path.exists(obj_path):
+                obj_path = None
+
+            mtl_path = path.replace(".json", ".mtl")
+            if not os.path.exists(mtl_path):
+                mtl_path = None
+
+            case = TestCase(
+                root=root_dir,
+                fbx_path=fbx_path,
+                obj_path=obj_path,
+                mtl_path=mtl_path,
+                title=desc["title"],
+                author=desc["author"],
+                license=desc["license"],
+                url=desc["url"])
+
+            yield case
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser("check_dataset.py --root <root>")
+    parser.add_argument("--root", help="Root directory to search for .json files")
+    parser.add_argument("--host-url", help="URL where the files are hosted")
+    parser.add_argument("--exe", help="check_fbx.c executable")
+    argv = parser.parse_args()
+
+    cases = list(gather_dataset_tasks(root_dir=argv.root))
+
+    def fmt_url(path, root=""):
+        if root:
+            path = os.path.relpath(path, root)
+        path = path.replace("\\", "/")
+        return f"{argv.host_url}/{path}"
+
+    ok_count = 0
+    for case in cases:
+        print(f"-- '{case.title}' by '{case.author}' ({case.license}) --")
+        print(f"  source url: {case.url}")
+        if argv.host_url:
+            print(f"    .fbx url: {fmt_url(case.fbx_path, case.root)}")
+            if case.obj_path:
+                print(f"    .obj url: {fmt_url(case.obj_path, case.root)}")
+            if case.mtl_path:
+                print(f"    .mtl url: {fmt_url(case.mtl_path, case.root)}")
+
+        args = [argv.exe]
+        args.append(case.fbx_path)
+        if case.obj_path:
+            args += ["--obj", case.obj_path]
+        print("$ " + " ".join(args))
+
+        try:
+            subprocess.check_call(args)
+            print("SUCCESS")
+            ok_count += 1
+        except subprocess.CalledProcessError:
+            print("FAIL")
+    print(f"{ok_count}/{len(cases)}")
+
+    if ok_count < len(cases):
+        exit(1)
--- test/check_fbx.c
+++ test/check_fbx.c
@@ -1,5 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #ifdef _WIN32
 #define WIN32_LEAN_AND_MEAN
@@ -7,35 +10,72 @@
 #include <Windows.h>
 #endif
 
-static void ufbxt_assert_fail(const char *func, const char *file, size_t line, const char *msg)
+static void ufbxt_assert_fail_imp(const char *func, const char *file, size_t line, const char *msg)
 {
-	fprintf(stderr, "%s:%zu: %s(\"%s\") failed\n", file, line, func, msg);
+	fprintf(stderr, "%s:%zu: %s(%s) failed\n", file, line, func, msg);
 	exit(2);
 }
 
-#define ufbxt_assert(m_cond) do { if (!(m_cond)) ufbxt_assert_fail("ufbxt_assert", __FILE__, __LINE__, #m_cond); } while (0)
-#define ufbx_assert(m_cond) do { if (!(m_cond)) ufbxt_assert_fail("ufbx_assert", __FILE__, __LINE__, #m_cond); } while (0)
+#define ufbxt_assert_fail(file, line, msg) ufbxt_assert_fail_imp("ufbxt_assert_fail", file, line, msg)
+#define ufbxt_assert(m_cond) do { if (!(m_cond)) ufbxt_assert_fail_imp("ufbxt_assert", __FILE__, __LINE__, #m_cond); } while (0)
+#define ufbx_assert(m_cond) do { if (!(m_cond)) ufbxt_assert_fail_imp("ufbx_assert", __FILE__, __LINE__, #m_cond); } while (0)
+
+bool g_verbose = false;
 
 #include "../ufbx.h"
 #include "check_scene.h"
+#include "testing_utils.h"
 
 #ifdef _WIN32
-int wmain(int argc, wchar_t **argv)
+int wmain(int argc, wchar_t **wide_argv)
 #else
 int main(int argc, char **argv)
 #endif
 {
-	ufbxt_assert(argc == 2);
-#if _WIN32
-	char path[1024];
-	int res = WideCharToMultiByte(CP_UTF8, 0, argv[1], -1, path, sizeof(path), NULL, NULL);
-	ufbxt_assert(res > 0 && res < sizeof(path));
-#else
-	const char *path = argv[1];
+#ifdef _WIN32
+	char **argv = (char**)malloc(sizeof(char*) * argc);
+	ufbxt_assert(argv);
+	for (int i = 0; i < argc; i++) {
+		int res = WideCharToMultiByte(CP_UTF8, 0, wide_argv[i], -1, NULL, 0, NULL, NULL);
+		ufbxt_assert(res > 0);
+		size_t dst_size = (size_t)res + 1;
+		char *dst = (char*)malloc(dst_size);
+		ufbxt_assert(dst);
+		res = WideCharToMultiByte(CP_UTF8, 0, wide_argv[i], -1, dst, (int)dst_size, NULL, NULL);
+		ufbxt_assert(res > 0 && (size_t)res < dst_size);
+		argv[i] = dst;
+	}
 #endif
 
+	const char *path = NULL;
+	const char *obj_path = NULL;
+	const char *dump_obj_path = NULL;
+
+	for (int i = 1; i < argc; i++) {
+		if (!strcmp(argv[i], "-v")) {
+			g_verbose = true;
+		} else if (!strcmp(argv[i], "--obj")) {
+			if (++i < argc) obj_path = argv[i];
+		} else if (!strcmp(argv[i], "--dump-obj")) {
+			if (++i < argc) dump_obj_path = argv[i];
+		} else {
+			path = argv[i];
+		}
+	}
+
+	if (!path) {
+		fprintf(stderr, "Usage: check_fbx <file.fbx>\n");
+		return 1;
+	}
+
+	ufbx_load_opts opts = { 0 };
+	opts.evaluate_skinning = true;
+	opts.evaluate_caches = true;
+	opts.target_axes = ufbx_axes_right_handed_y_up;
+	opts.target_unit_meters = 0.01;
+
 	ufbx_error error;
-	ufbx_scene *scene = ufbx_load_file(path, NULL, &error);
+	ufbx_scene *scene = ufbx_load_file(path, &opts, &error);
 
 	if (!scene) {
 		char buf[1024];
@@ -84,6 +124,7 @@ int main(int argc, char **argv)
 			if (!strcmp(unknown->super_type.data, "PluginParameters")) continue;
 			if (!strcmp(unknown->super_type.data, "TimelineXTrack")) continue;
 			if (!strcmp(unknown->super_type.data, "GlobalShading")) continue;
+			if (!strcmp(unknown->super_type.data, "ControlSetPlug")) continue;
 			if (!strcmp(unknown->sub_type.data, "NodeAttribute")) continue;
 			if (!strcmp(unknown->type.data, "GroupSelection")) continue;
 			if (!strcmp(unknown->name.data, "ADSKAssetReferencesVersion3.0")) {
@@ -106,6 +147,58 @@ int main(int argc, char **argv)
 	ufbxt_assert(scene->metadata.exporter != UFBX_EXPORTER_UNKNOWN || known_unknown);
 
 	ufbxt_check_scene(scene);
+
+	ufbxt_assert(scene->markers.count == 0);
+
+	if (obj_path) {
+		size_t obj_size;
+		void *obj_data = ufbxt_read_file(obj_path, &obj_size);
+		if (!obj_data) {
+			fprintf(stderr, "Failed to read .obj file: %s\n", obj_path);
+			return 1;
+		}
+
+		ufbxt_load_obj_opts obj_opts = { 0 };
+
+		ufbxt_obj_file *obj_file = ufbxt_load_obj(obj_data, obj_size, &obj_opts);
+
+		obj_file->normalize_units = true;
+
+		ufbx_scene *state;
+		if (obj_file->animation_frame >= 0) {
+			double time = (double)obj_file->animation_frame / (double)scene->settings.frames_per_second;
+			ufbx_evaluate_opts eval_opts = { 0 };
+			eval_opts.evaluate_skinning = true;
+			eval_opts.evaluate_caches = true;
+			state = ufbx_evaluate_scene(scene, &scene->anim, time, &eval_opts, NULL);
+			ufbxt_assert(state);
+		} else {
+			state = scene;
+			ufbx_retain_scene(state);
+		}
+
+		if (dump_obj_path) {
+			ufbxt_debug_dump_obj_scene(dump_obj_path, state);
+			printf("Dumped .obj to %s\n", dump_obj_path);
+		}
+
+		ufbxt_diff_error err = { 0 };
+		ufbxt_diff_to_obj(state, obj_file, &err, false);
+
+		if (err.num > 0) {
+			ufbx_real avg = err.sum / (ufbx_real)err.num;
+			printf("Absolute diff: avg %.3g, max %.3g (%zu tests)\n", avg, err.max, err.num);
+		}
+
+		ufbx_free_scene(state);
+		free(obj_file);
+		free(obj_data);
+	} else {
+		if (dump_obj_path) {
+			ufbxt_debug_dump_obj_scene(dump_obj_path, scene);
+			printf("Dumped .obj to %s\n", dump_obj_path);
+		}
+	}
 
 	ufbx_free_scene(scene);
 
