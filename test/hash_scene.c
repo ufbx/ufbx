4b2b52fd4df70dfdd57939d99ce0af674bb8b206
bqqbarbhg <samuli.1995@hotmail.com>
2022-06-19 17:49:50 +0300

    Hash scene with various allocation strategies

--- test/hash_scene.c
+++ test/hash_scene.c
@@ -5,13 +5,91 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <assert.h>
 
-ufbx_scene *load_scene(const char *filename, int frame)
+typedef struct {
+	size_t pos;
+	size_t size;
+	bool reverse;
+	char data[];
+} ufbxt_linear_alloactor;
+
+static void *linear_alloc_fn(void *user, size_t size)
+{
+	ufbxt_linear_alloactor *ator = (ufbxt_linear_alloactor*)user;
+	size = (size + 7u) & ~(size_t)7;
+
+	size_t pos = ator->pos;
+	if (ator->reverse) {
+		assert(pos >= size);
+		ator->pos = pos - size;
+		return ator->data + (pos - size);
+	} else {
+		assert(ator->size - pos >= size);
+		ator->pos = pos + size;
+		return ator->data + pos;
+	}
+}
+
+static void linear_free_allocator_fn(void *user)
+{
+	ufbxt_linear_alloactor *ator = (ufbxt_linear_alloactor*)user;
+	free(ator);
+}
+
+static void linear_allocator_init(ufbx_allocator *allocator, size_t size, bool reverse)
+{
+	ufbxt_linear_alloactor *ator = (ufbxt_linear_alloactor*)malloc(sizeof(ufbxt_linear_alloactor) + size);
+	assert(ator);
+	ator->size = size;
+	ator->reverse = reverse;
+	if (reverse) {
+		ator->pos = ator->size;
+	} else {
+		ator->pos = 0;
+	}
+
+	allocator->alloc_fn = &linear_alloc_fn;
+	allocator->free_allocator_fn = &linear_free_allocator_fn;
+	allocator->user = ator;
+}
+
+typedef struct {
+	bool dedicated_allocs;
+	const char *allocator;
+	size_t linear_allocator_size;
+} ufbxt_hasher_opts;
+
+static void setup_allocator(ufbx_allocator_opts *ator_opts, ufbxt_hasher_opts *opts)
+{
+	if (opts->dedicated_allocs) {
+		ator_opts->huge_threshold = 1;
+	}
+
+	size_t linear_size = opts->linear_allocator_size ? opts->linear_allocator_size : 128u*1024u*1024u;
+	if (opts->allocator) {
+		if (!strcmp(opts->allocator, "linear")) {
+			linear_allocator_init(&ator_opts->allocator, linear_size, false);
+		} else if (!strcmp(opts->allocator, "linear-reverse")) {
+			linear_allocator_init(&ator_opts->allocator, linear_size, true);
+		} else {
+			fprintf("Unknown allocator: %s\n", opts->allocator);
+			exit(1);
+		}
+	}
+}
+
+static ufbx_scene *load_scene(const char *filename, int frame, ufbxt_hasher_opts *hasher_opts)
 {
 	ufbx_load_opts opts = { 0 };
 	opts.load_external_files = true;
 	opts.evaluate_caches = true;
 	opts.evaluate_skinning = true;
+	opts.target_axes = ufbx_axes_right_handed_y_up;
+	opts.target_unit_meters = 1.0f;
+
+	if (hasher_opts->allocator) {
+	}
 
 	ufbx_error error;
 	ufbx_scene *scene = ufbx_load_file(filename, &opts, &error);
@@ -47,6 +125,7 @@ int main(int argc, char **argv)
 	bool do_check = false;
 	bool dump_all = false;
 	bool verbose = false;
+	ufbxt_hasher_opts hasher_opts = { 0 };
 
 	int frame = -1;
 	for (int i = 1; i < argc; i++) {
@@ -60,6 +139,10 @@ int main(int argc, char **argv)
 			if (++i < argc) dump_filename = argv[i];
 		} else if (!strcmp(argv[i], "--dump-all")) {
 			dump_all = true;
+		} else if (!strcmp(argv[i], "--dedicated-allocs")) {
+			hasher_opts.dedicated_allocs = true;
+		} else if (!strcmp(argv[i], "--allocator")) {
+			if (++i < argc) hasher_opts.allocator = argv[i];
 		} else if (!strcmp(argv[i], "--max-dump-errors")) {
 			if (++i < argc) max_dump_errors = atoi(argv[i]);
 		} else {
@@ -97,7 +180,7 @@ int main(int argc, char **argv)
 		uint64_t fbx_hash = 0;
 		char fbx_file[1024];
 		while (fscanf(f, "%" SCNx64 " %d %s", &fbx_hash, &frame, fbx_file) == 3) {
-			ufbx_scene *scene = load_scene(fbx_file, frame);
+			ufbx_scene *scene = load_scene(fbx_file, frame, &hasher_opts);
 
 			uint64_t hash = ufbxt_hash_scene(scene, NULL);
 			if (hash != fbx_hash || dump_all) {
@@ -133,7 +216,7 @@ int main(int argc, char **argv)
 			printf("%d/%d hashes match\n", num_total - num_fail, num_total);
 		}
 	} else {
-		ufbx_scene *scene = load_scene(filename, frame);
+		ufbx_scene *scene = load_scene(filename, frame, &hasher_opts);
 
 		if (dump_filename) {
 			dump_file = fopen(dump_filename, "wb");
