3571addb788c26c55047756a4a31ead04d8f8e4c
bqqbarbhg <samuli.1995@hotmail.com>
2022-11-01 15:53:36 +0200

    Add .mtl fuzz cases

--- test/test_fuzz.h
+++ test/test_fuzz.h
@@ -219,3 +219,71 @@ UFBXT_TEST(fuzz_obj_files)
 }
 #endif
 
+UFBXT_TEST(fuzz_mtl_files)
+#if UFBXT_IMPL
+{
+	size_t ok = 0;
+	size_t i = g_fuzz_step < SIZE_MAX ? g_fuzz_step : 0;
+	for (; i < 10000; i++) {
+		if (g_fuzz_file != SIZE_MAX && i != g_fuzz_file) continue;
+
+		char name[512];
+		char buf[512];
+		snprintf(name, sizeof(name), "mtl_fuzz_%04zu", i);
+		snprintf(buf, sizeof(buf), "%smtl_fuzz/fuzz_%04zu.mtl", data_root, i);
+
+		size_t size;
+		void *data = ufbxt_read_file(buf, &size);
+		if (!data) break;
+
+		ufbx_error error;
+
+		ufbx_load_opts load_opts = { 0 };
+		load_opts.temp_allocator.memory_limit = 0x4000000; // 64MB
+		load_opts.result_allocator.memory_limit = 0x4000000; // 64MB
+		load_opts.file_format = UFBX_FILE_FORMAT_MTL;
+
+		ufbx_scene *scene = ufbx_load_memory(data, size, &load_opts, &error);
+		if (scene) {
+			ufbxt_check_scene(scene);
+			ok++;
+		}
+
+		bool allow_error = scene == NULL;
+
+		ufbx_free_scene(scene);
+
+		ufbxt_progress_ctx stream_progress_ctx = { 0 };
+
+		bool temp_freed = false, result_freed = false;
+
+		ufbx_load_opts stream_opts = load_opts;
+		ufbxt_init_allocator(&stream_opts.temp_allocator, &temp_freed);
+		ufbxt_init_allocator(&stream_opts.result_allocator, &result_freed);
+		stream_opts.read_buffer_size = 1;
+		stream_opts.temp_allocator.huge_threshold = 1;
+		stream_opts.result_allocator.huge_threshold = 1;
+		stream_opts.progress_cb.fn = &ufbxt_measure_progress;
+		stream_opts.progress_cb.user = &stream_progress_ctx;
+		stream_opts.progress_interval_hint = 1;
+		ufbx_scene *streamed_scene = ufbx_load_file(buf, &stream_opts, &error);
+		if (streamed_scene) {
+			ufbxt_check_scene(streamed_scene);
+			ufbxt_assert(scene);
+		} else {
+			ufbxt_assert(!scene);
+		}
+		ufbx_free_scene(streamed_scene);
+
+		ufbxt_assert(temp_freed);
+		ufbxt_assert(result_freed);
+
+		ufbxt_do_fuzz(name, data, size, buf, allow_error, UFBX_FILE_FORMAT_MTL);
+
+		free(data);
+	}
+
+	ufbxt_logf(".. Loaded fuzz files: %zu (%zu non-errors)", i, ok);
+}
+#endif
+
