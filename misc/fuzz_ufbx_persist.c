e08494b2b3d0ba875a4b40175d13dfbbcf787d2d
bqqbarbhg <samuli.1995@hotmail.com>
2022-11-05 01:10:28 +0200

    Add a NO_AFL define to fuzz_ufbx_persist.c
    This is useful if you want to try to reproduce non-deterministic crashes that arise when fuzzing.

--- misc/fuzz_ufbx_persist.c
+++ misc/fuzz_ufbx_persist.c
@@ -40,8 +40,13 @@ int main(int argc, char **argv)
 	opts.file_format = UFBX_FILE_FORMAT_FBX;
 #endif
 
+#if defined(NO_AFL)
+	size_t size = (size_t)read(0, g_buffer, sizeof(g_buffer));
+	for (size_t i = 0; i < 10000; i++) {
+#else
 	while (__AFL_LOOP(10000)) {
 		size_t size = (size_t)read(0, g_buffer, sizeof(g_buffer));
+#endif
 
 		ufbx_scene *scene = ufbx_load_memory(g_buffer, size, &opts, NULL);
 		if (scene) {
