908b7fb69d4fcadc91d57999e75097ffdb2bb09e
bqqbarbhg <samuli.1995@hotmail.com>
2022-12-03 22:56:16 +0200

    Add -Wconversion-null to warnings test

--- misc/run_tests.py
+++ misc/run_tests.py
@@ -291,7 +291,7 @@ class GCCCompiler(Compiler):
             args += ["--sysroot", self.sysroot]
 
         if config.get("warnings", False):
-            args += ["-Wall", "-Wextra", "-Wsign-conversion", "-Wmissing-prototypes"]
+            args += ["-Wall", "-Wextra", "-Wsign-conversion", "-Wmissing-prototypes", "-Wconversion-null"]
             args += ["-Werror"]
 
         args.append("-g")
