cdaae34a5ff33b5509021f7990165b247a43a9a1
bqqbarbhg <bqqbarbhg@gmail.com>
2023-03-06 20:26:23 +0200

    Add tests for various C++ pointer types

--- test/cpp/cpp_test_pointer.h
+++ test/cpp/cpp_test_pointer.h
@@ -36,3 +36,61 @@ UFBXT_CPP_TEST(test_shared_deleter)
     std::shared_ptr<ufbx_scene> scene3 = std::move(scene2);
 }
 
+UFBXT_CPP_TEST(test_mesh_ptr)
+{
+    ufbx_unique_ptr<ufbx_scene> scene{ufbx_load_file(data_path("maya_cube_7500_ascii.fbx"), nullptr, nullptr)};
+	ufbxt_assert(scene);
+
+    ufbx_node *node = ufbx_find_node(scene.get(), "pCube1");
+    ufbxt_assert(node && node->mesh);
+    ufbx_mesh *mesh = node->mesh;
+
+    ufbx_unique_ptr<ufbx_mesh> mesh1{ufbx_subdivide_mesh(mesh, 1, nullptr, nullptr)};
+    ufbx_shared_ptr<ufbx_mesh> mesh2{ufbx_subdivide_mesh(mesh, 1, nullptr, nullptr)};
+    std::unique_ptr<ufbx_mesh, ufbx_deleter> mesh3{ufbx_subdivide_mesh(mesh, 1, nullptr, nullptr)};
+    ufbxt_assert(mesh1);
+    ufbxt_assert(mesh2);
+    ufbxt_assert(mesh3);
+}
+
+UFBXT_CPP_TEST(test_line_curve_ptr)
+{
+    ufbx_unique_ptr<ufbx_scene> scene{ufbx_load_file(data_path("maya_nurbs_curve_form_7700_ascii.fbx"), nullptr, nullptr)};
+	ufbxt_assert(scene);
+
+    ufbx_node *node = ufbx_find_node(scene.get(), "circleOpen");
+    ufbx_nurbs_curve *curve = ufbx_as_nurbs_curve(node->attrib);
+
+    ufbx_unique_ptr<ufbx_line_curve> line1{ufbx_tessellate_nurbs_curve(curve, nullptr, nullptr)};
+    ufbx_shared_ptr<ufbx_line_curve> line2{ufbx_tessellate_nurbs_curve(curve, nullptr, nullptr)};
+    std::unique_ptr<ufbx_line_curve, ufbx_deleter> line3{ufbx_tessellate_nurbs_curve(curve, nullptr, nullptr)};
+    ufbxt_assert(line1);
+    ufbxt_assert(line2);
+    ufbxt_assert(line3);
+}
+
+UFBXT_CPP_TEST(test_geometry_cache_ptr)
+{
+    ufbx_unique_ptr<ufbx_geometry_cache> cache1{ufbx_load_geometry_cache("marvelous_quad.xml", nullptr, nullptr)};
+    ufbx_shared_ptr<ufbx_geometry_cache> cache2{ufbx_load_geometry_cache("marvelous_quad.xml", nullptr, nullptr)};
+    std::unique_ptr<ufbx_geometry_cache, ufbx_deleter> cache3{ufbx_load_geometry_cache("marvelous_quad.xml", nullptr, nullptr)};
+	ufbxt_assert(cache1);
+}
+
+UFBXT_CPP_TEST(test_anim_ptr)
+{
+    ufbx_unique_ptr<ufbx_scene> scene{ufbx_load_file(data_path("maya_cube_7500_ascii.fbx"), nullptr, nullptr)};
+	ufbxt_assert(scene);
+
+    std::vector<uint32_t> layers = { scene->anim_layers[0]->typed_id };
+
+    ufbx_anim_opts opts = { };
+    opts.layer_ids = { layers.data(), layers.size() };
+
+    ufbx_unique_ptr<ufbx_anim> anim1{ufbx_create_anim(scene.get(), &opts, nullptr)};
+    ufbx_shared_ptr<ufbx_anim> anim2{ufbx_create_anim(scene.get(), &opts, nullptr)};
+    std::unique_ptr<ufbx_anim, ufbx_deleter> anim3{ufbx_create_anim(scene.get(), &opts, nullptr)};
+    ufbxt_assert(anim1);
+    ufbxt_assert(anim2);
+    ufbxt_assert(anim3);
+}
