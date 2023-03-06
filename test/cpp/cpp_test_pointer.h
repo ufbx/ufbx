074d77b1a47c5758a4d7de814d3f640de24fc849
bqqbarbhg <bqqbarbhg@gmail.com>
2023-03-06 20:37:18 +0200

    Test ufbx_ptr retain for all types as well

--- test/cpp/cpp_test_pointer.h
+++ test/cpp/cpp_test_pointer.h
@@ -47,10 +47,12 @@ UFBXT_CPP_TEST(test_mesh_ptr)
 
     ufbx_unique_ptr<ufbx_mesh> mesh1{ufbx_subdivide_mesh(mesh, 1, nullptr, nullptr)};
     ufbx_shared_ptr<ufbx_mesh> mesh2{ufbx_subdivide_mesh(mesh, 1, nullptr, nullptr)};
-    std::unique_ptr<ufbx_mesh, ufbx_deleter> mesh3{ufbx_subdivide_mesh(mesh, 1, nullptr, nullptr)};
+    ufbx_shared_ptr<ufbx_mesh> mesh3{mesh2};
+    std::unique_ptr<ufbx_mesh, ufbx_deleter> mesh4{ufbx_subdivide_mesh(mesh, 1, nullptr, nullptr)};
     ufbxt_assert(mesh1);
     ufbxt_assert(mesh2);
     ufbxt_assert(mesh3);
+    ufbxt_assert(mesh4);
 }
 
 UFBXT_CPP_TEST(test_line_curve_ptr)
@@ -63,18 +65,24 @@ UFBXT_CPP_TEST(test_line_curve_ptr)
 
     ufbx_unique_ptr<ufbx_line_curve> line1{ufbx_tessellate_nurbs_curve(curve, nullptr, nullptr)};
     ufbx_shared_ptr<ufbx_line_curve> line2{ufbx_tessellate_nurbs_curve(curve, nullptr, nullptr)};
-    std::unique_ptr<ufbx_line_curve, ufbx_deleter> line3{ufbx_tessellate_nurbs_curve(curve, nullptr, nullptr)};
+    ufbx_shared_ptr<ufbx_line_curve> line3{line2};
+    std::unique_ptr<ufbx_line_curve, ufbx_deleter> line4{ufbx_tessellate_nurbs_curve(curve, nullptr, nullptr)};
     ufbxt_assert(line1);
     ufbxt_assert(line2);
     ufbxt_assert(line3);
+    ufbxt_assert(line4);
 }
 
 UFBXT_CPP_TEST(test_geometry_cache_ptr)
 {
     ufbx_unique_ptr<ufbx_geometry_cache> cache1{ufbx_load_geometry_cache("marvelous_quad.xml", nullptr, nullptr)};
     ufbx_shared_ptr<ufbx_geometry_cache> cache2{ufbx_load_geometry_cache("marvelous_quad.xml", nullptr, nullptr)};
-    std::unique_ptr<ufbx_geometry_cache, ufbx_deleter> cache3{ufbx_load_geometry_cache("marvelous_quad.xml", nullptr, nullptr)};
+    ufbx_shared_ptr<ufbx_geometry_cache> cache3{cache2};
+    std::unique_ptr<ufbx_geometry_cache, ufbx_deleter> cache4{ufbx_load_geometry_cache("marvelous_quad.xml", nullptr, nullptr)};
 	ufbxt_assert(cache1);
+	ufbxt_assert(cache2);
+	ufbxt_assert(cache3);
+	ufbxt_assert(cache4);
 }
 
 UFBXT_CPP_TEST(test_anim_ptr)
@@ -89,8 +97,10 @@ UFBXT_CPP_TEST(test_anim_ptr)
 
     ufbx_unique_ptr<ufbx_anim> anim1{ufbx_create_anim(scene.get(), &opts, nullptr)};
     ufbx_shared_ptr<ufbx_anim> anim2{ufbx_create_anim(scene.get(), &opts, nullptr)};
-    std::unique_ptr<ufbx_anim, ufbx_deleter> anim3{ufbx_create_anim(scene.get(), &opts, nullptr)};
+    ufbx_shared_ptr<ufbx_anim> anim3{anim2};
+    std::unique_ptr<ufbx_anim, ufbx_deleter> anim4{ufbx_create_anim(scene.get(), &opts, nullptr)};
     ufbxt_assert(anim1);
     ufbxt_assert(anim2);
     ufbxt_assert(anim3);
+    ufbxt_assert(anim4);
 }
