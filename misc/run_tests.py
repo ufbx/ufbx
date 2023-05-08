b1ba97fbda01d0f23b397e7d9dc4ae01230d64bd
bqqbarbhg <bqqbarbhg@gmail.com>
2023-05-08 21:37:48 +0300

    run_tests.py: Run tests with 32-bit floats

--- misc/run_tests.py
+++ misc/run_tests.py
@@ -858,6 +858,15 @@ async def main():
         }
         target_tasks += compile_permutations("runner", runner_config, all_configs, ["-d", "data"])
 
+        float_config = {
+            "sources": ["test/runner.c", "ufbx.c"],
+            "output": "runner_float" + exe_suffix,
+            "defines": {
+                "UFBX_REAL_IS_FLOAT": 1,
+            }
+        }
+        target_tasks += compile_permutations("runner_float", float_config, arch_configs, ["-d", "data"])
+
         cpp_config = {
             "sources": ["misc/test_build.cpp"],
             "output": "cpp" + exe_suffix,
