e66be8fc68654acf28f5ee0c4d6371115db50ed1
bqqbarbhg <bqqbarbhg@gmail.com>
2024-01-09 20:29:35 +0200

    Switch back to C coverage

--- misc/generate_coverage.sh
+++ misc/generate_coverage.sh
@@ -8,10 +8,7 @@ LLVM_COV="${LLVM_COV:-llvm-cov}"
 LLVM_GCOV=$(realpath misc/llvm_gcov.sh)
 chmod +x misc/llvm_gcov.sh
 
-# clang -lm -coverage -g -std=gnu99 -DNDEBUG -DUFBX_VOID_ASSERT -DUFBX_DEV=1 -DUFBX_REGRESSION=1 -DUFBXT_THREADS=1 -pthread ufbx.c test/runner.c -o build/cov-runner
-
-clang++ -coverage -c -g -std=c++11 -DUFBX_COVERAGE_LCOV -DUFBX_DEV=1 -DUFBX_REGRESSION=1 -DUFBXT_THREADS=1 -x c++ ufbx.c -o build/ufbx-cov.o
-clang -lm -coverage -g -std=gnu99 -DUFBX_COVERAGE_LCOV -DUFBX_DEV=1 -DUFBX_REGRESSION=1 -DUFBXT_THREADS=1 -pthread build/ufbx-cov.o test/runner.c -o build/cov-runner
+clang -lm -coverage -g -std=gnu99 -DNDEBUG -DUFBX_DEV=1 -DUFBX_REGRESSION=1 -DUFBXT_THREADS=1 -pthread ufbx.c test/runner.c -o build/cov-runner
 
 build/cov-runner -d data
 $LLVM_COV gcov ufbx runner -b
