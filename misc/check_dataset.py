a248fce1d29b980fadc8effd7536150f6b608571
bqqbarbhg <bqqbarbhg@gmail.com>
2024-04-14 04:36:29 +0300

    Bump dataset date

--- misc/check_dataset.py
+++ misc/check_dataset.py
@@ -11,7 +11,7 @@ import datetime
 import asyncio
 import asyncio.subprocess
 
-LATEST_SUPPORTED_DATE = "2024-04-08"
+LATEST_SUPPORTED_DATE = "2024-04-15"
 
 class TestModel(NamedTuple):
     fbx_path: str
