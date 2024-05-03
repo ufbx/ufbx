2524c503c8d7c14a5a1f978788fdb4fe89b4b507
bqqbarbhg <bqqbarbhg@gmail.com>
2024-05-03 03:47:39 +0300

    check_dataset: Oops, set mtime before use

--- misc/check_dataset.py
+++ misc/check_dataset.py
@@ -162,6 +162,7 @@ def create_dataset_task(root_dir, root, filename, heavy, allow_unknown, last_sup
         append_unique_opt(options, "bake", [False, True])
 
     skip = False
+    mtime = os.path.getmtime(path)
     if last_supported_time and mtime > latest_supported_time.timestamp():
         skip = True
 
@@ -192,7 +193,6 @@ def create_dataset_task(root_dir, root, filename, heavy, allow_unknown, last_sup
             options["ignore-missing-external"] = [True]
         elif not skip:
             raise RuntimeError(f"Unknown feature: {feature}")
-    mtime = os.path.getmtime(path)
 
     models = []
     extra_files = []
