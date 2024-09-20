7b0232cdb4cd954446f504002d84d8ac2a1a57d9
bqqbarbhg <bqqbarbhg@gmail.com>
2024-09-20 19:29:29 +0300

    ufbx_libc_os: Posix implementation

--- misc/ufbx_libc_os.c
+++ misc/ufbx_libc_os.c
@@ -7,8 +7,6 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
-#if !defined(UFBX_NO_MALLOC)
-
 static uint64_t ufbxi_win32_total_memory = 0;
 
 volatile LONG ufbxi_win32_lock_init;
@@ -52,13 +50,11 @@ bool ufbx_malloc_os_free(void *pointer, size_t allocated_size)
 	if (ufbxi_win32_total_memory >= 32*1024*1024) {
 		ufbxi_win32_total_memory -= allocated_size;
 		VirtualFree(pointer, 0, MEM_RELEASE);
-		return TRUE;
+		return true;
 	} else {
-		return FALSE;
+		return false;
 	}
 }
-
-#endif
 
 #define UFBXI_WIN32_MAX_FILENAME_WCHAR 1024
 
@@ -110,6 +106,83 @@ void ufbx_stdio_close(void *file)
 {
 	HANDLE h = (HANDLE)file;
 	CloseHandle(h);
+}
+
+#else
+
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <pthread.h>
+
+static pthread_mutex_t ufbxi_posix_lock = PTHREAD_MUTEX_INITIALIZER;
+static uint64_t ufbxi_posix_total_memory = 0;
+
+void ufbx_malloc_os_lock()
+{
+	pthread_mutex_lock(&ufbxi_posix_lock);
+}
+
+void ufbx_malloc_os_unlock()
+{
+	pthread_mutex_unlock(&ufbxi_posix_lock);
+}
+
+void *ufbx_malloc_os_allocate(size_t size, size_t *p_allocated_size)
+{
+	size_t min_size = 4*1024*1024;
+	size_t alloc_size = size > min_size ? size : min_size;
+	void *pointer = mmap(NULL, alloc_size, PROT_READ|PROT_WRITE, MAP_ANONYMOUS |MAP_SHARED, -1, 0);
+	if (!pointer) return NULL;
+
+	ufbxi_posix_total_memory += alloc_size;
+
+	*p_allocated_size = alloc_size;
+	return pointer;
+}
+
+bool ufbx_malloc_os_free(void *pointer, size_t allocated_size)
+{
+	if (ufbxi_posix_total_memory >= 32*1024*1024) {
+		ufbxi_posix_total_memory -= allocated_size;
+		munmap(pointer, allocated_size);
+		return true;
+	} else {
+		return false;
+	}
+}
+
+void *ufbx_stdio_open(const char *path, size_t path_len)
+{
+	int fd = open(path, O_RDONLY);
+	if (fd < 0) return NULL;
+
+	return (void*)(uintptr_t)fd;
+}
+
+size_t ufbx_stdio_read(void *file, void *data, size_t size)
+{
+	int fd = (int)(uintptr_t)file;
+	ssize_t result = read(fd, data, size);
+	return result >= 0 ? result : SIZE_MAX;
+}
+
+bool ufbx_stdio_skip(void *file, size_t size)
+{
+	int fd = (int)(uintptr_t)file;
+	off_t result = lseek(fd, (off_t)size, SEEK_CUR);
+	return result >= 0;
+}
+
+uint64_t ufbx_stdio_size(void *file)
+{
+	return 0;
+}
+
+void ufbx_stdio_close(void *file)
+{
+	int fd = (int)(uintptr_t)file;
+	close(fd);
 }
 
 #endif
