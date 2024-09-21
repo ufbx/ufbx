23a6eaba62c58f3e13b2ee0117a3b505396fa54c
bqqbarbhg <bqqbarbhg@gmail.com>
2024-09-22 01:25:44 +0300

    Allow using generate_hashes.py out of tree

--- misc/generate_hashses.py
+++ misc/generate_hashses.py
@@ -13,14 +13,18 @@ if __name__ == "__main__":
 
     parser = ArgumentParser("generate_hashes.py --exe <exe> -o hashes.txt")
     parser.add_argument("--verbose", action="store_true", help="Show output")
+    parser.add_argument("--no-mtl", action="store_true", help="Do not hash .mtl files")
+    parser.add_argument("--path", default="data", help="Path to generate hashes from")
     parser.add_argument("--exe", required=True, help="hash_scene.c executable")
     parser.add_argument("-o", required=True, help="Output file path")
     argv = parser.parse_args()
 
     with open(argv.o, "wt") as f:
-        for root, dirs, files in os.walk("data"):
+        for root, dirs, files in os.walk(argv.path):
             for file in files:
                 path = os.path.join(root, file).replace("\\", "/")
+                if path.startswith("./"):
+                    path = path[2:]
                 if not is_ascii(path): continue
                 if "_fail_" in path: continue
                 if "/fuzz/" in path: continue
@@ -46,7 +50,7 @@ if __name__ == "__main__":
                             print(line)
                         print(line, file=f)
                         prev_output = output
-                elif file.endswith(".obj") or file.endswith(".mtl"):
+                elif file.endswith(".obj") or (not argv.no_mtl and file.endswith(".mtl")):
                     args = [argv.exe, path]
 
                     frame = 0
