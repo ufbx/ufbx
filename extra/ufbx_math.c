6a9ea17d3977afd6c669df727c4fa0cdb2650d0c
bqqbarbhg <bqqbarbhg@gmail.com>
2025-03-23 13:05:52 +0200

    ufbx_math fixes

--- extra/ufbx_math.c
+++ extra/ufbx_math.c
@@ -1941,52 +1941,6 @@ ufbx_math_abi double ufbx_nextafter(double x, double y)
 	return ufbxm_from_bits(hx, lx);
 }
 
-double ufbx_floor(double x)
-{
-	static const double huge = 1.0e300;
-	
-	ufbxm_int i0,i1,j0;
-	ufbxm_uint i,j;
-	ufbxm_bits xb = ufbxm_to_bits(x);
-	i0 = xb.hi;
-	i1 = (ufbxm_int)xb.lo;
-	j0 = ((i0>>20)&0x7ff)-0x3ff;
-	if(j0<20) {
-		if(j0<0) { 	/* raise inexact if x != 0 */
-			if(huge+x>0.0) {/* return 0*sign(x) if |x|<1 */
-				if(i0>=0) {i0=i1=0;} 
-				else if(((i0&0x7fffffff)|i1)!=0)
-				{ i0=0xbff00000;i1=0;}
-			}
-		} else {
-			i = (0x000fffff)>>j0;
-			if(((i0&i)|i1)==0) return x; /* x is integral */
-			if(huge+x>0.0) {	/* raise inexact flag */
-				if(i0<0) i0 += (0x00100000)>>j0;
-				i0 &= (~i); i1=0;
-			}
-		}
-	} else if (j0>51) {
-		if(j0==0x400) return x+x;	/* inf or NaN */
-		else return x;		/* x is integral */
-	} else {
-		i = ((unsigned)(0xffffffff))>>(j0-20);
-		if((i1&i)==0) return x;	/* x is integral */
-		if(huge+x>0.0) { 		/* raise inexact flag */
-			if(i0<0) {
-				if(j0==20) i0+=1; 
-				else {
-					j = i1+(1<<(52-j0));
-					if(j<i1) i0 +=1 ; 	/* got a carry */
-					i1=j;
-				}
-			}
-			i1 &= (~i);
-		}
-	}
-	return ufbxm_from_bits(i0, (ufbxm_uint)i1);
-}
-
 ufbx_math_abi double ufbx_ceil(double x)
 {
 	static const double huge = 1.0e300;
@@ -2115,7 +2069,7 @@ ufbx_math_abi double ufbx_rint(double x)
 	return w - TWO52[sx];
 }
 
-double ufbx_fmod(double x, double y)
+ufbx_math_abi double ufbx_fmod(double x, double y)
 {
 	static const double one = 1.0, zero[] = {0.0, -0.0};
 	
@@ -2135,7 +2089,7 @@ double ufbx_fmod(double x, double y)
 	
 	/* purge off exception values */
 	if((hy|ly)==0||(hx>=0x7ff00000)||	/* y=0,or x not finite */
-	((hy|((ly|-ly)>>31))>0x7ff00000))	/* or y is NaN */
+	((hy|((ly|(ufbxm_uint)-(ufbxm_int)ly)>>31))>0x7ff00000))	/* or y is NaN */
 	return (x*y)/(x*y);
 	if(hx<=hy) {
 		if((hx<hy)||(lx<ly)) return x;	/* |x|<|y| return x */
