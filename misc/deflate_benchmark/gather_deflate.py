30b43c8d5b48ed991822ee30efcdbcc2226899cd
bqqbarbhg <bqqbarbhg@gmail.com>
2025-11-05 22:07:08 +0200

    Gather deflate for write

--- misc/deflate_benchmark/gather_deflate.py
+++ misc/deflate_benchmark/gather_deflate.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import subprocess
+import json
 
 parser = argparse.ArgumentParser(description="Gather DEFLATE compressed streams from .fbx files")
 parser.add_argument("--exe", help="Executable path for per-file gathering, see `gather_deflate_main.cpp`")
@@ -14,8 +15,34 @@ for root,_,files in os.walk(argv.root):
     for file in files:
         if not file.endswith(".fbx"): continue
         path = os.path.join(root, file)
+        path = path.replace("\\", "/")
         print(path)
-        data += subprocess.check_output([argv.exe, path])
 
-with open(argv.o, "wb") as f:
-    f.write(data)
+        rel_base = os.path.splitext(path)[0]
+        rel_base = os.path.relpath(rel_base, argv.root)
+        dst_path = os.path.join(argv.o, rel_base)
+        dst_path = dst_path.replace("\\", "/")
+
+        os.makedirs(os.path.dirname(dst_path), exist_ok=True)
+
+        try:
+            subprocess.check_output([argv.exe, path, dst_path])
+        except Exception as e:
+            print(f"FAIL: {e}")
+
+total_json = []
+
+for root,_,files in os.walk(argv.o):
+    for file in files:
+        if not file.endswith(".json"): continue
+        if file == "index.json": continue
+        path = os.path.join(root, file)
+        with open(path, "rt", encoding="utf-8") as f:
+            data = json.load(f)
+            if data["arrays"]:
+                total_json.append(data)
+
+with open(os.path.join(argv.o, "index.json"), "wt", encoding="utf-8") as f:
+    json.dump({
+        "data": total_json,
+    }, f, indent=4)
--- misc/deflate_benchmark/gather_deflate_main.cpp
+++ misc/deflate_benchmark/gather_deflate_main.cpp
@@ -4,11 +4,14 @@
 #include <vector>
 #include <string>
 #include <stdio.h>
+#include <unordered_map>
 
-#ifdef _WIN32
-	#include <fcntl.h>
-	#include <io.h>
-#endif
+const char *g_root;
+std::unordered_map<std::string, uint32_t> g_counters;
+FILE *g_json;
+FILE *g_data;
+size_t g_data_offset;
+size_t g_array_count;
 
 std::vector<char> read_file(const char *path)
 {
@@ -22,8 +25,23 @@ std::vector<char> read_file(const char *path)
 	return data;
 }
 
-void dump_deflate_arrays(fbxdom::node_ptr node)
+void dump_deflate_arrays(fbxdom::node_ptr node, const std::string& path)
 {
+	std::string name;
+	std::string full_name;
+	for (char c : node->name) {
+		if (isalnum(c)) {
+			name += (char)tolower(c);
+		}
+		if (c >= 0x20 && c <= 0x7d && c != '\\') {
+			full_name += c;
+		}
+	}
+
+	std::string child_path = path;
+	if (!full_name.empty())
+		child_path += "/" + full_name;
+
 	for (fbxdom::value &value : node->values) {
 		if (value.data_array.encoding == 1) {
 			uint32_t decompressed_size = 0;
@@ -33,29 +51,75 @@ void dump_deflate_arrays(fbxdom::node_ptr node)
 			case 'l': case 'd': decompressed_size = value.data_array.length * 8; break;
 			}
 
-			fwrite(&value.data_array.compressed_length, 4, 1, stdout);
-			fwrite(&decompressed_size, 4, 1, stdout);
+			if (decompressed_size <= 4096) {
+				continue;
+			}
 
-			fwrite(value.data.data(), 1, value.data.size(), stdout);
+			uint32_t count = g_counters[name]++;
+
+			char filename[256];
+			snprintf(filename, sizeof(filename), "%s/%s_%u.zlib", g_root, name.c_str(), count);
+
+			fprintf(g_json, "\t\t{\n"
+				"\t\t\t\"type\": \"%c\",\n"
+				"\t\t\t\"path\": \"%s\",\n"
+				"\t\t\t\"size_u\": %u,\n"
+				"\t\t\t\"size_z\": %u,\n"
+				"\t\t\t\"offset\": %zu\n"
+				"\t\t},\n",
+				value.type,
+				child_path.c_str(),
+				decompressed_size,
+				value.data_array.compressed_length,
+				g_data_offset);
+
+			fwrite(value.data.data(), 1, value.data.size(), g_data);
+			g_data_offset += value.data.size();
+			g_array_count++;
 		}
 	}
 
 	for (fbxdom::node_ptr &child : node->children) {
-		dump_deflate_arrays(child);
+		dump_deflate_arrays(child, child_path);
 	}
 }
 
 int main(int argc, char **argv)
 {
+	g_root = argv[2];
+	const char *filename = argv[1];
+
 	std::vector<char> data = read_file(argv[1]);
 	fbxdom::node_ptr root = fbxdom::parse(data.data(), data.size());
 	if (!root) return 0;
 
-#ifdef _WIN32
-	_setmode(_fileno(stdout), O_BINARY);
-#endif
+	char json_path[256];
+	snprintf(json_path, sizeof(json_path), "%s.json", g_root);
+	g_json = fopen(json_path, "wb");
 
-	dump_deflate_arrays(root);
+	char data_path[256];
+	snprintf(data_path, sizeof(data_path), "%s.bin", g_root);
+	g_data = fopen(data_path, "wb");
+
+	fprintf(g_json, "{\n"
+		"\t\"source_path\": \"%s\",\n"
+		"\t\"data_path\": \"%s\",\n"
+		"\t\"arrays\": [\n",
+		filename,
+		data_path);
+
+	dump_deflate_arrays(root, "");
+
+	if (g_array_count > 0) {
+		// Remove trailing comma
+		fseek(g_json, -2, SEEK_CUR);
+	}
+
+	fprintf(g_json, "\n\t]\n"
+		"}\n");
+
+	fclose(g_json);
+	fclose(g_data);
 
 	return 0;
 }
